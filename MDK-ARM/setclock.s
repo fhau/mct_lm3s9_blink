7683addf48305268584761f5724c40dfa7e576bc
Franz Haunstetter <franz.haunstetter@gmail.com>
2013-09-08 13:06:41 +0200

    Change system clock to 66.7 MHz

--- MDK-ARM/setclock.s
+++ MDK-ARM/setclock.s
@@ -9,6 +9,9 @@
 ;  *********************************************
 
 SYSCTL_RCC_R          EQU 0x400FE060  ; Main run-mode clock config register
+SYSCTL_RCC_SYSDIV_M   EQU 0x07800000  ; System Clock Divisor
+SYSCTL_RCC_USESYSDIV  EQU 0x00400000  ; Enable System Clock Divider
+SYSCTL_RCC_SYSDIV_S   EQU 23          ; System Clock Divider shift
 SYSCTL_RCC_PWRDN      EQU 0x00002000  ; PLL Power Down
 SYSCTL_RCC_BYPASS     EQU 0x00000800  ; PLL Bypass
 SYSCTL_RCC_XTAL_M     EQU 0x000007C0  ; Crystal Value
@@ -34,14 +37,15 @@ SYSCTL_RCC2_BYPASS2   EQU 0x00000800  ; PLL Bypass 2
 SetClockXTAL
         ;
         ; Sets the external clock source for synchronizing the system clock
-		; by disabling RCC2 and setting RCC for PLL bypass / 16 MHz
+		; by disabling RCC2 and setting RCC for PLL 16 MHz -> 400 MHz -> 66.7 MHz output
         ;
         LDR     R0, = SYSCTL_RCC_R
 		LDR     R1, [R0]
-		AND     R1, #:NOT:(SYSCTL_RCC_XTAL_M)
-		AND     R1, #:NOT:(SYSCTL_RCC_OSCSRC_M:OR:SYSCTL_RCC_IOSCDIS:OR:SYSCTL_RCC_MOSCDIS)
-		ORR     R1, #SYSCTL_RCC_PWRDN:OR:SYSCTL_RCC_BYPASS:OR:SYSCTL_RCC_XTAL_16MHZ
-		ORR     R1, #SYSCTL_RCC_OSCSRC_MAIN:OR:SYSCTL_RCC_IOSCDIS
+		AND     R1, #:NOT:(SYSCTL_RCC_XTAL_M:OR:SYSCTL_RCC_BYPASS:OR:SYSCTL_RCC_PWRDN)
+		AND     R1, #:NOT:(SYSCTL_RCC_OSCSRC_M:OR:SYSCTL_RCC_MOSCDIS)
+		AND     R1, #:NOT:(SYSCTL_RCC_SYSDIV_M)
+		ORR     R1, #(2:SHL:SYSCTL_RCC_SYSDIV_S):OR:SYSCTL_RCC_USESYSDIV
+		ORR     R1, #SYSCTL_RCC_OSCSRC_MAIN:OR:SYSCTL_RCC_XTAL_16MHZ
 		STR     R1, [R0]
 		
         LDR     R0, = SYSCTL_RCC2_R
